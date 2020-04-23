1f2caba809f9d30cecec126fef4e3aea1e99004a
Dev <dvptl68@gmail.com>
2020-04-23 11:58:38 -0400

    Began implementation of view class

--- src/GUICalcViewClass.java
+++ src/GUICalcViewClass.java
@@ -1,3 +1,8 @@
+import java.awt.event.ActionEvent;
+
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+
 /**
  * View class.
  *
@@ -6,4 +11,62 @@
  */
 public class GUICalcViewClass implements GUICalcView {
 
+    /**
+     * Controller object to observse interaction events.
+     */
+    private GUICalcController controller;
+
+    /**
+     * Test area.
+     */
+    private final JTextArea display;
+
+    /**
+     * Operator buttons.
+     */
+    private final JButton clear, enter, add, subtract, multiply, divide,
+            leftParen, rightParen;
+
+    /**
+     * Number buttons.
+     */
+    private final JButton[] numbers;
+
+    /**
+     * Default Constructor.
+     */
+    public GUICalcViewClass() {
+
+        /*
+         * Create widgets
+         */
+        this.display = new JTextArea();
+        this.clear = new JButton("Clear");
+        this.enter = new JButton("Enter");
+        this.add = new JButton("+");
+        this.subtract = new JButton("-");
+        this.multiply = new JButton("*");
+        this.divide = new JButton("/");
+        this.leftParen = new JButton("(");
+        this.rightParen = new JButton(")");
+        this.numbers = new JButton[10];
+        for (int i = 0; i < this.numbers.length; i++) {
+            this.numbers[i] = new JButton(i + "");
+        }
+    }
+
+    @Override
+    public void registerObserver(GUICalcController controller) {
+        this.controller = controller;
+    }
+
+    @Override
+    public void updateDisplay(String s) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+
+    }
 }
