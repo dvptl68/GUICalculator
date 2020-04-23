46c3382b5cb384360a747cca9e1f593723d700ac
Dev <dvptl68@gmail.com>
2020-04-23 11:45:42 -0400

    Implemented model class

--- src/GUICalcModelClass.java
+++ src/GUICalcModelClass.java
@@ -6,4 +6,25 @@
  */
 public class GUICalcModelClass implements GUICalcModel {
 
+    /**
+     * Model variable.
+     */
+    private String display;
+
+    /**
+     * Default constructor.
+     */
+    public GUICalcModelClass() {
+        this.display = "";
+    }
+
+    @Override
+    public String display() {
+        return this.display;
+    }
+
+    @Override
+    public void setDisplay(String s) {
+        this.display = s;
+    }
 }
